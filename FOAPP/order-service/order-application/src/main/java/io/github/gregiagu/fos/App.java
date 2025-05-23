9cf6c400988ea3a2650e8c3a991bfbe35f509e23
gregiagu <69273611+gregiagu@users.noreply.github.com>
2025-05-23 04:59:30 -0300

    Skeleton project

---
+++ FOAPP/order-service/order-application/src/main/java/io/github/gregiagu/fos/App.java
@@ -0,0 +1,10 @@
+package io.github.gregiagu.fos;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
